65cbdd5c5641d40f7d03c9713a5f96fd3aa3f8b2
Matusevich Vitaliy <vimatu@gmail.com>
2022-06-10 18:53:59 +0300

    feat: implement hashHandlers

--- src/handlers/hashHandlers.js
+++ src/handlers/hashHandlers.js
@@ -1,9 +1,25 @@
-import { join, resolve } from 'path';
-import { readdir, stat } from 'fs/promises';
-import { checkIsDirectory, send } from '../utils/common.js';
-import { failedOperationMessage, invalidInputMassage } from '../utils/messages.js';
+import { resolve } from 'path';
+import { send } from '../utils/common.js';
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
 
 export const handleHash = async ({ path, args }) => {
-  // todo ***
+  const [pathToFile] = args;
+  const readStream = createReadStream(resolve(path, pathToFile));
+  const hash = createHash('sha256');
+
+  await new Promise((resolve, reject) => {
+    readStream.on('readable', () => {
+      const data = readStream.read();
+      if (data) hash.update(data);
+      else {
+        send(hash.digest('hex'));
+        resolve();
+      }
+    });
+
+    readStream.on('error', (err) => reject(err));
+  });
+
   return path;
 };
