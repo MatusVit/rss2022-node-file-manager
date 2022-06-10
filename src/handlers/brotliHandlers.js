262522f8c2b2a21d247901723da735eae3f8fc7a
Matusevich Vitaliy <vimatu@gmail.com>
2022-06-10 20:27:01 +0300

    feat: impltment brotliHandlers

--- src/handlers/brotliHandlers.js
+++ src/handlers/brotliHandlers.js
@@ -1,14 +1,25 @@
-import { join, resolve } from 'path';
-import { readdir, stat } from 'fs/promises';
-import { checkIsDirectory, send } from '../utils/common.js';
-import { failedOperationMessage, invalidInputMassage } from '../utils/messages.js';
+import { basename, resolve } from 'path';
+import { createBrotliCompress, createBrotliDecompress } from 'zlib';
+import { pipeline } from 'stream/promises';
+import { createReadStream, createWriteStream } from 'fs';
 
 export const handleCompress = async ({ path, args }) => {
-  // todo ***
+  const [pathToFile, pathToDestination] = args;
+  const readStream = createReadStream(resolve(path, pathToFile));
+  const writeStream = createWriteStream(resolve(path, pathToDestination, `${basename(pathToFile)}.br`));
+  const brotliCompress = createBrotliCompress();
+
+  await pipeline(readStream, brotliCompress, writeStream);
+
   return path;
 };
 
 export const handleDecompress = async ({ path, args }) => {
-  // todo ***
+  const [pathToFile, pathToDestination] = args;
+  const readStream = createReadStream(resolve(path, pathToFile));
+  const writeStream = createWriteStream(resolve(path, pathToDestination, basename(pathToFile).slice(0, -3)));
+  const brotliCompress = createBrotliDecompress();
+
+  await pipeline(readStream, brotliCompress, writeStream);
   return path;
 };
